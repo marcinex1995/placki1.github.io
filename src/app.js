65af5431d67cefdfb3329d2c5e570f3d12eacb7e
marcinex1995 <38530835+marcinex1995@users.noreply.github.com>
2018-05-09 20:55:10 +0200

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const Peer = require('peerjs');
+const uid = require('uid');
+const $ = require('jquery')
+const openStream = require('./openStream');
+const playVideo = require('./playVideo');
+const config = {host: 'placki.herokuapp.com', port: 443, source: true, key: 'peerjs'}
+function getPeer(){
+    const id = uid(10);
+    $('#peer-id').append(id);
+    return id;
+}
+const peer = Peer(getPeer(), config);
+
+ $('#btnCall').click (() =>{ 
+ const friendId = $('#txtFriendId').val();
+openStream(stream => {
+ playVideo(stream, 'localStream');
+ const call = peer.call(friendId, stream);
+ console.log(call);
+ call.on('stream', remoteStream => playVideo(remoteStream, 'friendStream'));
+    });
+ });
+
+peer.on('call', (call) => {
+openStream(stream => {
+playVideo(stream, 'localStream');
+call.answer(stream);
+call.on('stream', remoteStream => playVideo(remoteStream, 'friendStream'));
+});
+ });
